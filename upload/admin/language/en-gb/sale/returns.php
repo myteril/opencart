757045bbf553d04902fd7d38ea0b9e7061de1cf5
myteril <myteril@gmail.com>
2023-08-26 12:54:27 +0300

    Fix sale/returns language file.

--- upload/admin/language/en-gb/sale/returns.php
+++ upload/admin/language/en-gb/sale/returns.php
@@ -62,4 +62,4 @@ $_['error_lastname']       = 'Last Name must be between 1 and 32 characters!';
 $_['error_email']          = 'E-Mail Address does not appear to be valid!';
 $_['error_telephone']      = 'Telephone must be between 3 and 32 characters!';
 $_['error_product']        = 'Product Name must be greater than 1 and less than 255 characters!';
-$_['error_model']          = 'Product Model must be greater than 1 and less than 64 characters!';
+$_['error_model']          = 'Product Code must be greater than 1 and less than 64 characters!';
