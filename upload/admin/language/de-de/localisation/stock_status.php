3798a23f440a649408070e51bad6d1f6d9343380
myteril <myteril@gmail.com>
2023-08-27 17:53:50 +0300

    Add German language support for the item availability field of stock statuses.

--- upload/admin/language/de-de/localisation/stock_status.php
+++ upload/admin/language/de-de/localisation/stock_status.php
@@ -1,21 +1,38 @@
 <?php
 // Heading
-$_['heading_title']    = 'Lagerstand Status Informationen';
+$_['heading_title']    			= 'Lagerstand Status Informationen';
 
 // Text
-$_['text_success']     = 'Erfolgreich: Sie haben Lagerstands Status Information modifiziert!';
-$_['text_list']        = 'Lagerstand-Status Liste';
-$_['text_add']         = 'Neuer Lagerstand-Status anlegen';
-$_['text_edit']        = 'Lagerstand-Status bearbeiten';
+$_['text_success']     			= 'Erfolgreich: Sie haben Lagerstands Status Information modifiziert!';
+$_['text_list']       			= 'Lagerstand-Status Liste';
+$_['text_add']         			= 'Neuer Lagerstand-Status anlegen';
+$_['text_edit']        			= 'Lagerstand-Status bearbeiten';
 
 // Column
-$_['column_name']      = 'Lagerstand-Status';
-$_['column_action']    = 'Aktion';
+$_['column_name']      			= 'Lagerstand-Status';
+$_['column_action']    			= 'Aktion';
 
 // Entry
-$_['entry_name']       = 'Lagerstand-Status Titel';
+$_['entry_name']       			= 'Lagerstand-Status Titel';
+$_['entry_item_availability']   = 'Artikelverfügbarkeit (in Suchmaschinen)';
+
+// Values
+$_['value_backorder'] 			= 'Rückständige Bestellung';
+$_['value_discontinued'] 		= 'Auslaufartikel';
+$_['value_instock'] 			= 'Auf Lager';
+$_['value_instoreonly'] 		= 'Nur im Geschäft erhältlich';
+$_['value_limitedavailability'] = 'Begrenzte Verfügbarkeit';
+$_['value_onlineonly'] 			= 'Nur online';
+$_['value_outofstock'] 			= 'Ausverkauft';
+$_['value_preorder'] 			= 'Vorbestellen';
+$_['value_presale'] 			= 'Vorverkauf';
+$_['value_soldout'] 			= 'Ausverkauft';
 
 // Error
-$_['error_permission'] = 'Warnung: Sie haben keine Berechtigung Lagerstand-Status Information zu modifizieren!';
-$_['error_name']       = 'Der Lagerstand-Status Titel muss zwischen 3 und 32 Zeichen lang sein!';
-$_['error_product']    = 'Warnung: Dieser Lagerstand-Status kann nicht gelöscht werden, da er derzeit %s Produkten zugewiesen ist!';
+$_['error_permission'] 			= 'Warnung: Sie haben keine Berechtigung Lagerstand-Status Information zu modifizieren!';
+$_['error_name']       			= 'Der Lagerstand-Status Titel muss zwischen 3 und 32 Zeichen lang sein!';
+$_['error_product']    			= 'Warnung: Dieser Lagerstand-Status kann nicht gelöscht werden, da er derzeit %s Produkten zugewiesen ist!';
+$_['error_item_availability']   = 'Bitte wählen Sie einen Wert für die Artikelverfügbarkeit aus.';
+
+// Helper
+$_['help_item_availability'] 	= 'Der Lagerstatus, der in Suchmaschinen angezeigt wird.';
