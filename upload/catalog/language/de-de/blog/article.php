1eb805df7e7dee3847eb5dc03739f41956affe29
myteril <myteril@gmail.com>
2023-08-27 14:06:10 +0300

    Improve languages.

--- upload/catalog/language/de-de/blog/article.php
+++ upload/catalog/language/de-de/blog/article.php
@@ -4,7 +4,7 @@
 $_['text_blog']                 = 'Blog';
 $_['text_tags']               	= 'Tags';
 $_['text_author']              	= 'Autor';
-$_['text_added_at']           	= 'Hinzugefügt am %s';
+$_['text_added_at']           	= 'Veröffentlicht am %s';
 $_['text_modified_at']        	= 'Bearbeitet am %s';
 $_['text_about_author']        	= 'Über den Autor';
 $_['text_about_article']        = 'Über den Artikel';
--- upload/catalog/language/en-gb/blog/article.php
+++ upload/catalog/language/en-gb/blog/article.php
@@ -4,7 +4,7 @@
 $_['text_blog']                 = 'Blog';
 $_['text_tags']               	= 'Tags';
 $_['text_author']              	= 'Author';
-$_['text_added_at']           	= 'Added at %s';
+$_['text_added_at']           	= 'Published at %s';
 $_['text_modified_at']        	= 'Edited at %s';
 $_['text_about_author']        	= 'About Author';
 $_['text_about_article']        = 'About Article';
