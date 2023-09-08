0afaf7750b813674d0a3868ebec0f22b873c5f8a
myteril <myteril@gmail.com>
2023-09-08 15:48:57 +0300

    Add the German translation for the section `Feeds`

---
+++ upload/admin/language/de-de/marketing/feed.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']     	= 'Feeds';
+
+// Text
+$_['text_success']      	= 'Erfolg: Du hast den Feed aktualisiert!';
+$_['text_list']         	= 'Feed-Liste';
+$_['text_add']          	= 'Marketing-Tracking hinzufügen';
+$_['text_edit']         	= 'Marketing-Tracking bearbeiten';
+$_['text_all_languages'] 	= 'Alle Sprachen';
+
+// Column
+$_['column_store_name'] 	= 'Shopname';
+$_['column_feed_name']  	= 'Feedname';
+$_['column_language_name']  = 'Sprache';
+$_['column_feed_url']   	= 'Feed-URL';
+$_['column_last_update']	= 'Letzte Aktualisierung';
+$_['column_action']     	= 'Aktion';
+
+// Button
+$_['button_update_now'] 	= 'Jetzt aktualisieren';
+$_['button_copy_url']   	= 'Link kopieren';
